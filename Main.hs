686c96b11b25cf52e623fe7650f086b86decaca0
Ricky Elrod <ricky@elrod.me>
2015-03-12 16:21:40 -0400

    Outline of what needs to happen, but doesn't actually do it yet
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- Main.hs
+++ Main.hs
@@ -8,6 +8,7 @@ import Control.Monad (join)
 import Data.Aeson
 import Data.Aeson.Lens
 import qualified Data.HashMap.Strict as HM
+import Data.List (partition)
 import Data.Maybe (catMaybes)
 import Data.Scientific
 import qualified Data.Set as S
@@ -15,14 +16,17 @@ import qualified Data.Text as T
 import Network.Wreq
 
 recentCompletedUrl :: String
-recentCompletedUrl = "https://apps.fedoraproject.org/datagrepper/raw?delta=\
-                     \172800&topic=org.fedoraproject.prod.ansible.playbook.\
-                     \complete"
+--recentCompletedUrl = "https://apps.fedoraproject.org/datagrepper/raw?delta=\
+--                     \172800&topic=org.fedoraproject.prod.ansible.playbook.\
+--                     \complete"
 
 recentStartUrl :: String
-recentStartUrl = "https://apps.fedoraproject.org/datagrepper/raw?delta=\
-                 \172800&topic=org.fedoraproject.prod.ansible.playbook.\
-                 \start"
+--recentStartUrl = "https://apps.fedoraproject.org/datagrepper/raw?delta=\
+--                 \172800&topic=org.fedoraproject.prod.ansible.playbook.\
+--                 \start"
+
+recentCompletedUrl = "http://localhost:8000/out.json"
+recentStartUrl = "http://localhost:8000/out1.json"
 
 data Playbook = Updating | Rebooting deriving (Eq, Ord, Show)
 data MsgData = MsgData { _user :: T.Text
@@ -34,17 +38,19 @@ data PlaybookStatus = Start | Complete deriving (Eq, Ord, Show)
 data PlaybookRun = PlaybookRun { _msgdata :: MsgData
                                , _status :: PlaybookStatus
                                } deriving (Eq, Ord, Show)
+makeLenses ''MsgData
+makeLenses ''PlaybookRun
 
 valueToPlaybookRun :: Value -> Maybe PlaybookRun
 valueToPlaybookRun v = do
   topic <- join (startOrStop <$> v ^? key "topic" . _String)
   ts <- toRealFloat <$> v ^? key "timestamp" . _Number
-  user <- v ^? key "msg" . key "userid" . _String
-  playbook <- join (determinePlaybook <$> v ^? key "msg" . key "playbook" . _String)
+  user' <- v ^? key "msg" . key "userid" . _String
+  playbook' <- join (determinePlaybook <$> v ^? key "msg" . key "playbook" . _String)
   let target = case topic of
                 Start -> v ^.. key "msg" . key "extra_vars" . key "target" . _String
                 Complete -> v ^. key "msg" . key "results" . _Object . to HM.keys
-  return $ PlaybookRun (MsgData user playbook target ts) topic
+  return $ PlaybookRun (MsgData user' playbook' target ts) topic
 
   where
     determinePlaybook p =
@@ -69,6 +75,25 @@ getRecent s = do
 
 main :: IO ()
 main = do
-  recentStarted <- S.fromList <$> getRecent recentStartUrl
-  recentCompleted <- S.fromList <$> getRecent recentCompletedUrl
+  recentStarted <- getRecent recentStartUrl
+  recentCompleted <- getRecent recentCompletedUrl
+  let (updC, rebC) = partition (\x -> x ^. msgdata . playbook == Updating) recentCompleted
+      (updS, rebS) = partition (\x -> x ^. msgdata . playbook == Updating) recentStarted
+
+      -- We start with all starting playbook runs and all completed playbook runs.
+      -- For each starting playbook run, try to find a matching completed playbook run.
+      -- Do this by:
+      --   - Filtering completed playbook runs to find a run of the same type (update or reboot)
+      --   - Taking that list and filtering it to find targets that match the starting playbook
+      --   - Taking *that* list and filtering it to find completions that happened *after* the start time
+      --   - Map over it to get a set of hostnames
+      --   - If any of them match the starting playbook target, then return False and move on
+      --   - If none of them match, the starting playbook is still likely being run.
+      matchingPlaybookType p1 p2 = p1 ^. msgdata . playbook == p2 ^. msgdata . playbook
+      matchingPlaybookTargets p1 p2 = p1 ^. msgdata . hosts == p2 ^. msgdata . hosts
+      correctPlaybookTimestamps p1 p2 = p1 ^. msgdata . timestamp > p2 ^. msgdata . timestamp
+      playbookPredicate p1 p2 =
+        matchingPlaybookType p1 p2 &&
+        matchingPlaybookTargets p1 p2 &&
+        correctPlaybookTimestamps p1 p2
   error "hi!"
